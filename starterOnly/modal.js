ab9ac2ed1af9bace5d6ff23e867892ca3bc804af
david <davidcolmoulin974@gmail.com>
2023-09-20 19:17:56 +0400

    add function closeModal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,5 +1,5 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
+  let x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
@@ -12,12 +12,20 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+
+// launch modal form
+closeBtn.addEventListener ("click", closeModal );
+// close modal form
+function closeModal() {
+  modalBtn.style.display = "none";
 }
 
 
