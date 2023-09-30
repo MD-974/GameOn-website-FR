2ac93fcd74f53b3312db5db4100dc3260884d171
david <davidcolmoulin974@gmail.com>
2023-09-30 15:24:45 +0400

    :wastebasket: Delete file config.js

--- JS/modal.js
+++ JS/modal.js
@@ -6,20 +6,20 @@ function editNav() {
     x.className = "topnav";
   }
 }
-// // DOM Elements
-// const modalbg = document.querySelector(".bground");
-// const modalBtn = document.querySelectorAll(".modal-btn");
-// const formData = document.querySelectorAll(".formData");
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
 
-// const closeBtn = document.querySelector(".close");
+const closeBtn = document.querySelector(".close");
 
-// // FORM Elements
-// let first = document.getElementById("first"); 
-// let last = document.getElementById("last");
-// let email = document.getElementById("email");
-// let birthDate = document.getElementById("birthdate");
-// let quantity =document.getElementById("quantity");
-// let listBtnRadio = document.querySelectorAll("input[type=radio]");
+// FORM Elements
+let first = document.getElementById("first"); 
+let last = document.getElementById("last");
+let email = document.getElementById("email");
+let birthDate = document.getElementById("birthdate");
+let quantity =document.getElementById("quantity");
+let listBtnRadio = document.querySelectorAll("input[type=radio]");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
