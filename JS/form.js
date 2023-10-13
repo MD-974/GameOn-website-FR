a500525ba0541ae8489c313c2269e074e4a23c55
david <davidcolmoulin974@gmail.com>
2023-10-13 09:36:50 +0400

    :wrench: test pour bordure rouge sur les input(non concluant)

--- JS/form.js
+++ JS/form.js
@@ -4,16 +4,14 @@
 
   const firstNameInput = document.getElementById("first");
   const firstNameError = firstNameInput.parentElement;
+  // const firstNameBorder = firstNameInput.inputMode
 
   const lastNameInput = document.getElementById("last");
   const lastNameError = lastNameInput.parentElement;
-
   const emailInput = document.getElementById("email");
   const emailError = emailInput.parentElement;
-  
   const birthdateInput = document.getElementById("birthdate");
   const birthdateError = birthdateInput.parentElement;
-
   const quantityInput = document.getElementById("quantity");
   const quantityError = quantityInput.parentElement;
 
@@ -24,6 +22,9 @@
   const location5 =  document.getElementById("location5");
   const location6 =  document.getElementById("location6");
 
+  // const locations = document.querySelectorAll(type[radio])
+  // const locationsError=locations.parentElement;
+
   const checkbox1 = document.getElementById("checkbox1");
   const checkbox1Error = checkbox1.parentElement;
 
@@ -33,24 +34,20 @@
     event.preventDefault()
 
 
-    //--récupération de la valeur de l’input
+    //--récupération de la valeur de l’input "first"
     console.log(firstNameInput.value)
     //--vérification  des requis de validation
     if (firstNameInput.value == "") {
         console.log("Le Prénom est requis.");
-        // récuperation du dernier element de la div et modification du texte
-        firstNameError.lastElementChild.textContent = "Le Prénom est requis.";
-        // affichage du message d'erreur
-        firstNameError.lastElementChild.style.display = "block";
+        firstNameError.lastElementChild.textContent = "Le Prénom est requis.";// récuperation du dernier element de la div et modification du texte
+        firstNameError.lastElementChild.style.display = "block";// affichage du message d'erreur
+        // firstNameBorder.firstNameInput.style.border = "block";
       } else if (firstNameInput.value.length < 2) {
         console.log("Le Prénom doit faire plus de 2 caractères.");
-        // récuperation du dernier element de la div et modification du texte
-        firstNameError.lastElementChild.textContent="Le Prénom doit faire plus de 2 caractères.";
-        // affichage du message d'erreur
-        firstNameError.lastElementChild.style.display="block";
+        firstNameError.lastElementChild.textContent="Le Prénom doit faire plus de 2 caractères.";// récuperation du dernier element de la div et modification du texte
+        firstNameError.lastElementChild.style.display="block";// affichage du message d'erreur
       } else {
-        // cacher le message d'erreur
-        firstNameError.lastElementChild.style.display="none";
+        firstNameError.lastElementChild.style.display="none";// cacher le message d'erreur
       }
       
 
@@ -59,79 +56,73 @@
     //--vérification  des requis de validation
     if (lastNameInput.value == "") {
         console.log("Le Nom est requis.");
-       // récuperation du dernier element de la div et modification du texte
-        lastNameError.lastElementChild.textContent = "Le Nom est requis.";
-        // affichage du message d'erreur
-        lastNameError.lastElementChild.style.display = "block";
+        lastNameError.lastElementChild.textContent = "Le Nom est requis.";// récuperation du dernier element de la div et modification du texte
+        lastNameError.lastElementChild.style.display = "block";// affichage du message d'erreur
       } else if (lastNameInput.value.length < 2) {
         console.log("Le Nom doit faire plus de 2 caractères.");
-        // récuperation du dernier element de la div et modification du texte
-        lastNameError.lastElementChild.textContent="Le Nom doit faire plus de 2 caractères.";
-        // affichage du message d'erreur
-        lastNameError.lastElementChild.style.display="block";
+        lastNameError.lastElementChild.textContent="Le Nom doit faire plus de 2 caractères.";// récuperation du dernier element de la div et modification du texte
+        lastNameError.lastElementChild.style.display="block";// affichage du message d'erreur
       } else {
-        // cacher le message d'erreur
-        lastNameError.lastElementChild.style.display="none";
+        lastNameError.lastElementChild.style.display="none";// cacher le message d'erreur
       }
 
-    //--récupération de l'email
+    //--récupération de la valeur l'email
     const regexEmail = /^[A-Za-z0-9\.\-]+@([A-Za-z0-9\.\-]+\.[A-Za-z]{2,4})$/;
       console.log(emailInput.value)
-
+    //--vérification  des requis de validation  
     if (emailInput.value == "") {
       console.log("L'Email est requis.");
-      // récuperation du dernier element de la div et modification du texte
-      emailError.lastElementChild.textContent = "L'Email est requis.";
-      // affichage du message d'erreur
-      emailError.lastElementChild.style.display = "block";
+      emailError.lastElementChild.textContent = "L'Email est requis.";// récuperation du dernier element de la div et modification du texte
+      emailError.lastElementChild.style.display = "block";// affichage du message d'erreur
     } else if (!regexEmail.test(emailInput.value)){
       console.log("L'Email n'est pas au bon format.");
-      // récuperation du dernier element de la div et modification du texte
-      emailError.lastElementChild.textContent = "L'Email n'est pas au bon format ( exemple@email.fr ).";
-      // affichage du message d'erreur
-      emailError.lastElementChild.style.display = "block";
-    } else {
-      // cacher le message d'erreur
-      emailError.lastElementChild.style.display="none";
+      emailError.lastElementChild.textContent = "L'Email n'est pas au bon format ( exemple@email.fr ).";// récuperation du dernier element de la div et modification du texte
+      emailError.lastElementChild.style.display = "block";// affichage du message d'erreur
+    } else {     
+      emailError.lastElementChild.style.display="none";// cacher le message d'erreur
     }
 
 
     //--validation de la date de naissance
     const regexBirthdate = /^([0-9]{2})|([0-9]{2})|([0-9]{4})$/;
      console.log(birthdateInput.value)
-
+    //--vérification  des requis de validation
      if (birthdateInput.value == "") {
       console.log("Veuillez indiquer une date de naissance.");
-      // récuperation du dernier element de la div et modification du texte
-      birthdateError.lastElementChild.textContent = "Veuillez indiquer une date de naissance.";
-      // affichage du message d'erreur
-      birthdateError.lastElementChild.style.display = "block";
+      birthdateError.lastElementChild.textContent = "Veuillez indiquer une date de naissance.";// récuperation du dernier element de la div et modification du texte
+      birthdateError.lastElementChild.style.display = "block";// affichage du message d'erreur
     } else {
-      // cacher le message d'erreur
-      birthdateError.lastElementChild.style.display="none";
+      birthdateError.lastElementChild.style.display="none";// cacher le message d'erreur
     }
     
 
     //--validation nombre de participation
     const regexQuantity = /^[0-9]$/;
       console.log(quantityInput.value);
-      
+    //--vérification  des requis de validation  
       if (quantityInput.value == "") {
         console.log("Veuillez indiquer le nombre de participation entre 0-99.");
-      // récuperation du dernier element de la div et modification du texte
-      quantityError.lastElementChild.textContent = "Veuillez indiquer le nombre de participation entre 0-99.";
-      // affichage du message d'erreur
-      quantityError.lastElementChild.style.display = "block";
+      
+      quantityError.lastElementChild.textContent = "Veuillez indiquer le nombre de participation entre 0-99.";// récuperation du dernier element de la div et modification du texte
+      quantityError.lastElementChild.style.display = "block";// affichage du message d'erreur
     } else {
-      // cacher le message d'erreur
-      quantityError.lastElementChild.style.display="none";
+      quantityError.lastElementChild.style.display="none";// cacher le message d'erreur
     }
     
     //--validation selection de la ville
       if (!location1.checked && !location2.checked && !location3.checked
     && !location4.checked && !location5.checked && !location6.checked) { 
       console.log("Selectionner une ville."); 
-  }
+    }
+  //   if (!locations.checked){
+  //       // récuperation du dernier element de la div et modification du texte
+  //    locationsError.lastElementChild.textContent = "Veuillez selectionner une ville.";
+  //    // affichage du message d'erreur
+  //    locationsError.lastElementChild.style.display = "block";
+  //  } else {
+  //    // cacher le message d'erreur
+  //    locationsError.lastElementChild.style.display="none";
+  //  }
 
   //--validation condition d'utilisation
   if (!checkbox1.checked){
@@ -144,8 +135,6 @@
      // cacher le message d'erreur
      checkbox1Error.lastElementChild.style.display="none";
    }
-
-  
   } 
   )
 
